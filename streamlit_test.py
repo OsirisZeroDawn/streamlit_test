a48ed9b9784f30f9d40b70413584098a3b6abd0c
A <ashley@evatt.ai>
2025-11-12 15:37:52 +0800

    updating

--- streamlit_test.py
+++ streamlit_test.py
@@ -14,3 +14,6 @@ from copy import deepcopy
 #Data from: https://data.open-power-system-data.org/renewable_power_plants/
 clean_energy_ch = pd.read_csv("/Users/ashleyedgar/Desktop/Ashley Edgar Data Science/ashley-edgar-data-science/03_Visualisation/Day 3/Exercises/renewable_power_plants_CH.csv")
 clean_energy_ch.head()
+
+st.write("Hello World")
+st.write("New Change")
