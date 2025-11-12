16646949a86a10ace67c7d49e3d215bab4acb71e
A <ashley@evatt.ai>
2025-11-12 15:32:48 +0800

    adding relevant packages to get streamlit running

--- streamlit_test.py
+++ streamlit_test.py
@@ -1,2 +1,16 @@
 # Data Science Test Example
-# Let's check that this still works 
+# Let's check that this still works
+
+# Streamlit live coding script
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import plotly.express as px
+import plotly.graph_objects as go
+from urllib.request import urlopen
+import json
+from copy import deepcopy
+
+#Data from: https://data.open-power-system-data.org/renewable_power_plants/
+clean_energy_ch = pd.read_csv("/Users/ashleyedgar/Desktop/Ashley Edgar Data Science/ashley-edgar-data-science/03_Visualisation/Day 3/Exercises/renewable_power_plants_CH.csv")
+clean_energy_ch.head()
